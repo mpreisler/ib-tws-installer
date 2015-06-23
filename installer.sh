2fefcad91f9fac564ee4ba329c1d6b0bf969025e
Martin Preisler <martin@preisler.me>
2015-06-23 17:52:29 +0200

    Better stdout output when installing

--- installer.sh
+++ installer.sh
@@ -16,25 +16,32 @@ which jar || exit 1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TMPDIR=`mktemp -d`
 
-pushd "$TMPDIR"
+pushd "$TMPDIR" > /dev/null
 
 if $LATEST; then
-    wget -O unixmacosx.jar https://download2.interactivebrokers.com/download/unixmacosx_latest.jar
+    echo "Downloading the latest testing version..."
+    wget --no-verbose -O unixmacosx.jar https://download2.interactivebrokers.com/download/unixmacosx_latest.jar
 else
-    wget -O unixmacosx.jar https://download2.interactivebrokers.com/download/unixmacosx.jar
+    echo "Downloading the latest stable version..."
+    wget --no-verbose -O unixmacosx.jar https://download2.interactivebrokers.com/download/unixmacosx.jar
 fi
 
+echo "Extracting unixmacosx.jar..."
 jar xf unixmacosx.jar
 
 if $REPLACE_IF_EXISTS; then
+    echo "Removing previous version at /opt/IBJts..."
     rm -rf "/opt/IBJts"
 fi
 
+echo "Installing to /opt/IBJts..."
 mv IBJts /opt/
 
+echo "Copying ib-tws wrapper to /usr/bin..."
 cp "$DIR/ib-tws" /usr/bin/
+echo "Installing the desktop file..."
 desktop-file-install "$DIR/ib-tws.desktop"
 
-popd
+popd > /dev/null
 
 rm -r "$TMPDIR"
